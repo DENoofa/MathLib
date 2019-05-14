899fc32212a2bf7a9c48286574293b821924da1a
DENoofa <40660415+DENoofa@users.noreply.github.com>
2019-05-14 16:49:55 +1000

    Add files via upload

--- MathLab/MathLibrary/PipVector.cpp
+++ MathLab/MathLibrary/PipVector.cpp
@@ -9,45 +9,81 @@ public:
 
 };
 
-template <typename PipV3D>
+template <typename V3D>
 class PipVec3D
 {
 public:
-
-	union Vector3
+	//struct for the vector
+	struct
 	{
-		struct
-		{
-			float x, y, z;
-		};
-		struct
+		V3D my_vec3d[3];
 
-		{
-			float r, g, b;
-		};
-
-		float data[3];
+		V3D xi, yi, zi;
 	};
 
-	PipV3D Vector3::opeartor[](float m_index) const
-	{
-		switch (m_index)
-		{
-		
-		case 0: return x;
-		case 1: return y;
-		case 2: return z;
-		default: assert(false);
-		
-		}
-	}
+	PipVec3D(V3D x = 0, V3D y = 0, V3D z = 0) : xi(x), yi(y), zi(z) {};
 
-	Vector3 Vector3::opeartor + (const Vector3& other) const
+
+	float PipVec3D::operator [] (int index) const
 	{
-		return { x + other.x, y + other.y, z + other.z };
+		return my_vec3d[index];
 	}
 
 
+	PipVec3D::operator const float* () const
+	{
+		return &my_vec3d;
+	}
 
+	
+	//Addition
+	PipVec3D PipVec3D::operator + (const PipVec3D& other) const
+	{
+		return { xi + other.xi, yi + other.yi, zi + other.zi };
+	}
+
+	//Addition assigning
+	PipVec3D &PipVec3D::operator += (const PipVec3D *other)
+	{
+		return PipVec3D(xi + other->xi, yi + other->yi, zi + other->yi);
+	}
+
+	//subtraction
+	PipVec3D& PipVec3D::operator -= (const PipVec3D& other) const
+	{
+		xi -= other.xi;
+		yi -= other.yi;
+		zi -= other.yi;
+		return *this;
+	}
+
+	//subtraction assigning
+	PipVec3D &PipVec3D::operator - (const PipVec3D *other)
+	{
+		return PipVec3D(xi - other.xi, yi - other.yi, zi - other.zi);
+	}
+
+
+	//Scale sizing, in multiplication
+	PipVec3D PipVec3D::operator * (float scalar) const
+	{
+		return { xi * scalar, yi * scalar, zi * scalar };
+	}
+
+	//Scale sizing, in division
+	PipVec3D& PipVec3D::operator /= (float scalar) const
+	{
+		xi /= scalar;
+		yi /= scalar;
+		zi /= scalar;
+		return *this;
+	}
+
+	PipVec3D& PipVec3D::operator = (const PipVec3D& other)
+	{
+		xi = other.xi;
+		yi = other.yi;
+		zi = other.zi;
+	}
 };
 
